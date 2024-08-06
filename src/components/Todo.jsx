132f91ac05e3f306184aa000aceb6cfc933571e0
rishabhag18 <18r.agarwal@gmail.com>
2024-08-06 22:43:22 +0530

    First Commit of ToDo List Header Section Added

---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import "./ToDo.css";
+
+const Todo=()=>{
+    return (
+        <section className="todo-container">
+            <header>
+                <h1>ToDo List</h1>
+            </header>
+        </section>
+    )
+}
+export default Todo;
